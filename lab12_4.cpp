92ca66f112f7fffcf2064ff90e5e415714ac83d9
patiphan24 <patiphan_leknok@cmu.ac.th>
2023-12-29 15:56:22 +0700

    lab12

--- lab12_4.cpp
+++ lab12_4.cpp
@@ -2,6 +2,10 @@
 
 using namespace std;
 
+template <typename T>
+
+void mySwap(T&,T&);
+
 int main(){
 	int x, y;
 	string a, b;
@@ -30,3 +34,10 @@ int main(){
 	return 0;
 }
 
+template <typename T>
+void mySwap(T &x,T &y){
+    T i  = x;
+    x = y;
+    y = i;
+
+}
